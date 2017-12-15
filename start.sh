a6dd2348498cebb974c3d5bfda696bf6ce8c5faf
halidm <halid.mousa@gmail.com>
2017-12-15 22:29:27 +0400

    fine tuning start params

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 NODE=gw@$(hostname)
 APP=gateway
 cd `dirname $0`
-exec erl -smp auto +P 134217727 +K true +A 64 -pa $PWD/_build/default/lib/*/ebin -boot start_sasl -s lager -s $APP $@ -config app.config -sname $NODE -setcookie chat_cookie
+exec erl -smp auto +P 134217727 +K true +A 64 +stbt ts +zdbbl 512MB -pa $PWD/_build/default/lib/*/ebin -boot start_sasl -s lager -s $APP $@ -config app.config -sname $NODE -setcookie chat_cookie
